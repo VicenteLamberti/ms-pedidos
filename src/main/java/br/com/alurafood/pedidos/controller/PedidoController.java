266239baaad4e5ef35248c8e68eb2790fadd0e9b
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2022-11-28 09:01:29 -0300

    Load Balance

--- src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
+++ src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
@@ -1,17 +1,26 @@
 package br.com.alurafood.pedidos.controller;
 
-import br.com.alurafood.pedidos.dto.PedidoDto;
-import br.com.alurafood.pedidos.dto.StatusDto;
-import br.com.alurafood.pedidos.service.PedidoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
+import java.util.List;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.net.URI;
-import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.alurafood.pedidos.dto.PedidoDto;
+import br.com.alurafood.pedidos.dto.StatusDto;
+import br.com.alurafood.pedidos.service.PedidoService;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -56,5 +65,9 @@ public class PedidoController {
 
             return ResponseEntity.ok().build();
 
+        }
+        @GetMapping("/porta")
+        public String retornarPorta(@Value("${local.server.port}")String porta) {
+        	return String.format("Requisicao respondida %s", porta);
         }
 }
